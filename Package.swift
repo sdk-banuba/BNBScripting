424701e14448d731a3a890621a83fa044e49cda2
macOS CI <sdk.accounts@banuba.com>
2023-12-21 14:26:20 +0300

    1.9.4-99-gef5d08583

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.9.3-95-g54f78c5d8b"
+let bnbPackageVersion: Version = "1.9.4-99-gef5d08583"
 
 let package = Package(
     name: "BNBScripting",
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBScripting",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.3-95-g54f78c5d8b/BNBScripting.zip",
-            checksum: "fd0976d40f71370912cb5a28a1d633d4b44f5568ff2bcdbf6cc9f17ef771c7e4"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.4-99-gef5d08583/BNBScripting.zip",
+            checksum: "34964a65685fb4fb03c64405d5d0ae5a99a5360e47a4fabbf5747cb4bb29b418"
         ),
     ]
 )
