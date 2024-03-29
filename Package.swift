5a789ccf5b43cb6e5c6305a529c2a3ce0626c927
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-03-29 09:29:48 +0300

    1.11.1-70-g2e0f1ab74e

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.11.1-70-g2e0f1ab74"
+let bnbPackageVersion: Version = "1.11.1-70-g2e0f1ab74e"
 
 let package = Package(
     name: "BNBScripting",
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBScripting",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.11.1-70-g2e0f1ab74/BNBScripting.zip",
-            checksum: "34903d820f0088ab0777c551fed3b218e1534809bbf6ec96c8b0bdffd4b98310"
+            url: "None",
+            checksum: "c788cd66a4574c06f8c80ee623ea769bb8911b2bcbe75c07fdf26285e45145c3"
         ),
     ]
 )
