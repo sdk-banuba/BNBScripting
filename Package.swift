1c7a486dec12a6b2ace7003efdd22da9d5365b45
ci <ci@banuba.com>
2024-11-04 20:00:29 +0300

    1.14.1-174-gec051bdc7

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-173-gec24d1c86"
+let bnbPackageVersion: Version = "1.14.1-174-gec051bdc7"
 
 let package = Package(
     name: "BNBScripting",
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBScripting",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-173-gec24d1c86/BNBScripting.zip",
-            checksum: "b9e99093412e199217f2159b4183aafe59c1de107f0810fc145caae8963fdb7f"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-174-gec051bdc7/BNBScripting.zip",
+            checksum: "dd6aeb73654fa4ed1513d4802f08958b819799c799698d7b54273bdea3154715"
         ),
     ]
 )
