34c9df4242ef342bfe7a29a4446a908a48a5c71c
ci <ci@banuba.com>
2024-12-17 22:52:11 +0300

    1.16.3

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .binaryTarget(
             name: "BNBScripting",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.16.3/BNBScripting.zip",
-            checksum: "1f2f99697a4fb042cb77c8e1988ceff015b9e9bd3354c90c589a04a17a548ce1"
+            checksum: "967cccb680168c2acae25b3bae9bf6908312d98283f2d959977ed839c380667e"
         ),
     ]
 )
